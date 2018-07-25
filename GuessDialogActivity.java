403aa7447d93bf89489d9fd92688d705de3aa1a7
chocychang <a0953903449@gmail.com>
2018-07-25 11:50:54 +0800

    Update GuessDialogActivity.java

--- GuessDialogActivity.java
+++ GuessDialogActivity.java
@@ -1,9 +1,11 @@
 package com.edu.ncu.drawlandmark;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Handler;
@@ -11,12 +13,13 @@ import android.os.Handler;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
-public class GuessDialogActivity extends AppCompatActivity {
+public class GuessDialogActivity extends Activity {
 
     private TextView tv_image;
     private TextView tv_text;
 
     public  boolean guess_status;
+    String solving;
 
 
     public GuessDialogActivity(){
@@ -32,11 +35,29 @@ public class GuessDialogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guessdialog);
+        setContentView(R.layout.activity_guess_dialog);
 
         this.tv_image = (TextView) this.findViewById(R.id.image_dia);
         this.tv_text = (TextView) this.findViewById(R.id.text_dia);
 
+        Intent intent = getIntent();
+        this.guess_status = intent.getBooleanExtra("GuessStatus_EXTRA",TRUE);
+        this.solving = intent.getStringExtra("Solve_MSG");
+
+        setDialog();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(GuessDialogActivity.this, SolveActivity.class);
+                intent.putExtra("Solving_MSG", solving);
+                startActivity(intent);
+                GuessDialogActivity.this.finish();
+
+            }}, 1500);
 
     }
 
@@ -49,29 +70,39 @@ public class GuessDialogActivity extends AppCompatActivity {
         //this.guess_status=guess_status;
         //修正
         if(guess_status) {
-            tv_image.setText("X");
-            tv_image.setTextColor(getResources().getColor(R.color.lightPink));
-            tv_text.setText("答錯了！");
-            tv_text.setTextColor(getResources().getColor(R.color.lightPink));
-        } else {
             tv_image.setText("O");
             tv_image.setTextColor(getResources().getColor(R.color.lightGreen));
             tv_text.setText("答對了！");
             tv_text.setTextColor(getResources().getColor(R.color.lightGreen));
 
+        } else {
+
+            tv_image.setText("X");
+            tv_image.setTextColor(getResources().getColor(R.color.lightPink));
+            tv_text.setText("答錯了！");
+            tv_text.setTextColor(getResources().getColor(R.color.lightPink));
+
         }
 
+    }
 
+    public void setDialog(){
+        if(guess_status) {
+            tv_image.setText("O");
+            tv_image.setTextColor(getResources().getColor(R.color.lightGreen));
+            tv_text.setText("答對了！");
+            tv_text.setTextColor(getResources().getColor(R.color.lightGreen));
+
+        } else {
+
+            tv_image.setText("X");
+            tv_image.setTextColor(getResources().getColor(R.color.lightPink));
+            tv_text.setText("答錯了！");
+            tv_text.setTextColor(getResources().getColor(R.color.lightPink));
+
+        }
     }
 
 
-    public boolean setCorrectStatus(){
-        guess_status = FALSE;
-        return guess_status;
-    }
-
-    public boolean setWrongStatus(){
-        guess_status = TRUE;
-        return guess_status;
-    }
 }
+
